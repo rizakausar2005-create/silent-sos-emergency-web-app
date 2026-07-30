8d77ad3533dd741ab81d5ac7810e7d3c916931f9
rizakausar2005-create <rizakausar2005@gmail.com>
2026-07-30 20:13:27 +0530

    feat: add Express backend setup

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 5000;
+
+// Allows Express to read JSON data
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Silent SOS Backend is Running...");
+});
+
+// Register API
+app.post("/register", (req, res) => {
+
+    console.log(req.body);
+
+    res.json({
+        message: "User registered successfully!"
+    });
+
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
